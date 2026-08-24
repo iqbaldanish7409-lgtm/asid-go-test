879aa25bb2206e8ada46ddd8ccf45767da1e6a8a
iqbaldanish7409-lgtm <iqbaldanish7409@gmail.com>
2026-08-24 22:19:12 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,23 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import {
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
 
 const firebaseConfig = {
+
     apiKey: "AIzaSyBqmC4cAPLtJBTleDbEMZv7wj9vlkVbvv4",
+
     authDomain: "asid-go-3afd0.firebaseapp.com",
+
     projectId: "asid-go-3afd0",
+
     storageBucket: "asid-go-3afd0.firebasestorage.app",
+
     messagingSenderId: "129600064128",
+
     appId: "1:129600064128:web:f2998dc2519780aba9f465"
 };
 
